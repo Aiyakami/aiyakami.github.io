316c97dacaaf181492ef70f2c3011dc2fbd6a3cf
aiyakami <19806409967@qq.com>
2024-12-25 16:35:20 +0800

    Site updated: 2024-12-25 16:35:20

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1735114338841";
+const VERSION = "1735115717246";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
